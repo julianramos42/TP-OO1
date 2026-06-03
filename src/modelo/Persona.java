c76d32c0e77218a141d9a11d75400c890eeab653
GonzaLiberatori <gonzaliberatori24@gmail.com>
2026-06-03 16:22:21 -0300

    Clases creadas de personas: Persona(Abstracta,Cajero y Cocinero

---
+++ src/modelo/Persona.java
@@ -0,0 +1,124 @@
+package modelo;
+
+import java.time.LocalDate;
+import java.time.Period;
+
+public abstract class Persona {
+	
+	protected int idPersona;
+	protected String nombre;
+	protected String apellido;
+	protected long dni;
+	protected LocalDate fechaNacimiento;
+	protected LocalDate fechaIngreso;
+	protected double sueldoBase;
+	
+	
+	public Persona(int idPersona, String nombre, String apellido, long dni, LocalDate fechaNacimiento,
+			LocalDate fechaIngreso, double sueldoBase) {
+		super();
+		this.idPersona = idPersona;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.dni = dni;
+		this.fechaNacimiento = fechaNacimiento;
+		this.fechaIngreso = fechaIngreso;
+		this.sueldoBase = sueldoBase;
+	}
+
+
+	public int getIdPersona() {
+		return idPersona;
+	}
+
+
+	public void setIdPersona(int idPersona) {
+		this.idPersona = idPersona;
+	}
+
+
+	public String getNombre() {
+		return nombre;
+	}
+
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+	public String getApellido() {
+		return apellido;
+	}
+
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+
+	public long getDni() {
+		return dni;
+	}
+
+
+	public void setDni(long dni) {
+		this.dni = dni;
+	}
+
+
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+
+	public LocalDate getFechaIngreso() {
+		return fechaIngreso;
+	}
+
+
+	public void setFechaIngreso(LocalDate fechaIngreso) {
+		this.fechaIngreso = fechaIngreso;
+	}
+
+
+	public double getSueldoBase() {
+		return sueldoBase;
+	}
+
+
+	public void setSueldoBase(double sueldoBase) {
+		this.sueldoBase = sueldoBase;
+	}
+
+
+	@Override
+	public String toString() {
+		return "\nPersona [idPersona=" + idPersona + ", nombre=" + nombre + ", apellido=" + apellido + ", dni=" + dni
+				+ ", fechaNacimiento=" + fechaNacimiento + ", fechaIngreso=" + fechaIngreso + ", sueldoBase="
+				+ sueldoBase + "]";
+	}
+	
+	public boolean equals(Persona persona) {
+		
+		return persona.getIdPersona() == this.dni;
+	}
+	
+	public int calcularEdad() {
+		
+		return Period.between(this.fechaNacimiento, LocalDate.now()).getYears();
+	}
+	
+	public int validarAntiguedad(LocalDate fechaActual) {
+		
+		return Period.between(this.fechaIngreso, fechaActual).getYears();
+	}
+	
+	public abstract double calcularSueldo();
+
+}
